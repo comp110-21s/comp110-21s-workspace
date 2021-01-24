ae60a354f903ebd4f8350cb167e489060d3f1672
Yujia Shi <yuyujia@email.unc.edu>
2021-01-25 02:15:28 +0800

    FINISHED EX00

--- exercises/ex00_hello_world.py
+++ exercises/ex00_hello_world.py
@@ -1,8 +1,6 @@
-"""My first program for COMP110"""
+"""My first program for COMP110."""
 
-_author_ = "730433234"
+__author__ = "730433234"
 print("Hello, world.")
 print("I hope I can get A in COMP110")
 print("See You")
-
-
