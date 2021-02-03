57937d77e992197853d5d0b896cdd89d95dadc2e
Kris Jordan <krisjordan@gmail.com>
2021-02-03 14:32:19 -0500

    Add skeleton code for exercise 1

---
+++ exercises/ex01/fortune_cookie.py
@@ -0,0 +1,15 @@
+"""Program that outputs one of at least four random, good fortunes."""
+
+__author__ = "ENTER YOUR 9-DIGIT PID HERE"
+
+# The randint function is imported from the random library so that
+# you are able to generate integers at random.
+# 
+# Documentation: https://docs.python.org/3/library/random.html#random.randint
+#
+# For example, consider the function call expression: randint(1, 100)
+# It will evaluate to an int value >= 1 and <= 100. 
+from random import randint
+
+
+# Begin your solution here...
---
+++ exercises/ex01/vaccine_calc.py
@@ -0,0 +1,20 @@
+"""A vaccination calculator."""
+
+__author__ = "YOUR PID HERE"
+
+# The datetime data type is imported from the datetime library.
+# A datetime object models a specific date and time.
+#
+# Official Documentation: https://docs.python.org/3/library/datetime.html#datetime-objects
+from datetime import datetime
+
+# The timedelta data type is imported from the timedelta library.
+# A timedelta object models a "time span", such as 1 day or 1 hour and 3 minutes.
+# Subtracting two datetime objects will result in the timedelta between them.
+# Adding a datetime and a timedelta will result in the datetime offset by the timedelta.
+#
+# Official Documentation: https://docs.python.org/3/library/datetime.html#datetime.timedelta
+from datetime import timedelta
+
+
+# Begin your solution here...
