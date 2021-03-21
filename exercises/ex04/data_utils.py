3a0f0d1cabab4077ca998a164d90f07364d4fbce
Kris Jordan <krisjordan@gmail.com>
2021-03-21 15:16:48 -0400

    Exercise 04 added to course repo.

---
+++ exercises/ex04/data_utils.py
@@ -0,0 +1,18 @@
+"""Utility functions for wrangling data."""
+
+__author__ = "YOUR PID HERE"
+
+
+from csv import DictReader
+
+
+def read_csv_rows(csv_file: str) -> list[dict[str, str]]:
+    """Read a CSV file's contents into a list of rows."""
+    rows: list[dict[str, str]] = []
+    
+    # TODO 0.1) Complete the implementation of this function here.
+
+    return rows
+
+
+# TODO: Define the other functions here.
