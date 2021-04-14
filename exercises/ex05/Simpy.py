64e73c8fd7aa02a6411f507164ab543551a628c2
Kris Jordan <kris@cs.unc.edu>
2021-04-14 13:07:23 -0400

    Add exercise 05 starter code.

---
+++ exercises/ex05/Simpy.py
@@ -0,0 +1,13 @@
+"""Utility class for numerical operations."""
+
+from __future__ import annotations
+
+from typing import Union
+
+__author__ = "YOUR PID HERE"
+
+
+class Simpy:
+    values: list[float]
+
+    # TODO: Your constructor and methods will go here.
